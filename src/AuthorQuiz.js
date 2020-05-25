0395c054a30b10a4c599f851e3b4edb164ef5e92
Szilvi Horvath <szilvi.horvath@centrica.com>
2020-05-25 21:52:31 +0100

    first commit

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './bootstrap.min.css';
+
+function AuthorQuiz() {
+  return (
+    <div>
+      Author Quiz
+    </div>
+  );
+}
+
+export default AuthorQuiz;
