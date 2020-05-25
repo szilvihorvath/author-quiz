8525601bb8f4fd44ea73399dd54528a75a9315a1
Szilvi Horvath <szilvi.horvath@centrica.com>
2020-05-25 22:53:26 +0100

    installed enzyme and added first basic test

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import ReactDOM from 'react-dom';
+import AuthorQuiz from './AuthorQuiz';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("Author Quiz", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<AuthorQuiz />, div);
+  })
+})
