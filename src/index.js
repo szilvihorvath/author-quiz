b9c18295b451ebb84421c3793987f6419bff9818
Szilvi Horvath <szilvi.horvath@centrica.com>
2020-06-27 22:40:05 +0100

    added component event to select answer and set background colour based on answer being correct or not

--- src/index.js
+++ src/index.js
@@ -78,13 +78,26 @@ function getTurnData() {
 };
 
 const state = {
-  turnData: getTurnData(authors)
+  turnData: getTurnData(authors),
+  highlight: ''
 };
 
-ReactDOM.render(
-    <AuthorQuiz {...state} />,
+function onAnswerSelected(answer) {
+  const isCorrect = state.turnData.author.books.some((book) => book === answer);
+  
+  state.highlight = isCorrect ? 'correct' : 'wrong';
+  
+  render();
+};
+
+function render() {
+  ReactDOM.render(
+    <AuthorQuiz {...state} onAnswerSelected={onAnswerSelected} />,
   document.getElementById('root')
-);
+  );
+};
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
